7eb24d0a4aaf58d5b938979db90750c74762fcb0
LUNDI19 <michaelmcloughlin9@gmail.com>
2021-07-13 12:53:01 +0200

    basic testing library

---
+++ spec/note_spec.js
@@ -0,0 +1,11 @@
+it('stores a new note', function() {
+});
+
+it('returns a note', function() {
+});
+
+it('returns the first 20 characters of the note', function() {
+});
+
+it('has an ID', function() {
+});
