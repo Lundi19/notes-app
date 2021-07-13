691619275a6ba2cf94daf15dd7b33916bbb354ae
LUNDI19 <michaelmcloughlin9@gmail.com>
2021-07-13 20:59:48 +0200

    a note can appear after submit

--- src/interface.js
+++ src/interface.js
@@ -1,16 +1,22 @@
+document.addEventListener("DOMContentLoaded", function() {
 
-document.addEventListener("DOMContentLoaded", ()=> {
-  var note = new Note
-  console.log(note)
 
-  document.querySelector("#exampleFormControlTextarea1").addEventListener('submit',()=>{
-    const enteredNote = document.getElementById("#exampleFormControlTextarea1").value
+
+  let submitText = document.querySelector('#add-note');
+  
+  submitText.addEventListener('submit', (event) => {
     event.preventDefault();
-
-    note = new Note(enteredNote)
-    note.getNote()
-    console.log(note)
+    let inputText = document.querySelector("#exampleFormControlTextarea1").value;
+    let newNote = createNote(inputText);
+    document.querySelector('#note-list').innerText = newNote.getNote();
   })
-
-
-})
+  
+  function createNote(inputText) {
+    let newNote = new Note(inputText);
+    return newNote;
+  }
+  
+  
+  
+  
+  });
