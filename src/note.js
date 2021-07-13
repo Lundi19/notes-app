c7a35d185c93ddaead23ad965062550895fd8fb4
LUNDI19 <michaelmcloughlin9@gmail.com>
2021-07-13 16:20:51 +0200

    passing first 3 tests no interface.js logic

--- src/note.js
+++ src/note.js
@@ -1,5 +1,18 @@
+var nextID = 0;
+
 class Note {
-  constructor() {
-  this.text = "test"
+  constructor(text) {
+    this.id = nextID++;
+    this.text = text
   }
+
+  getNote() {
+    return this.text
+  }
+
+  firstTwenty() {
+    return this.text.slice(0, 20)
+  }
+
+  // displayAll()
 };
