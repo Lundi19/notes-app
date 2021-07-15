e8a850f67a3c49e95fdfec0ebce409c15716eee1
mikey-roberts <mikey_roberts@outlook.com>
2021-07-15 11:30:58 +0100

    slice notes down to 20 chars

--- src/interface.js
+++ src/interface.js
@@ -28,12 +28,7 @@ window.onload = displayNotes();
     div.classList.add("notes-list")
     div.setAttribute("data-id", "newNote.id")
     div.innerHTML = `
-    <span style="display:inline-block;
-    white-space: nowrap;
-    overflow: hidden;
-    text-overflow: ellipsis;
-    max-width: 25ch;">
-    <h3>${newNote.text}</h3></span>
+    <h3>${newNote.text.slice(0,20)}</h3></span>
     <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-box-arrow-up-right" viewBox="0 0 16 16">
           <path fill-rule="evenodd" d="M8.636 3.5a.5.5 0 0 0-.5-.5H1.5A1.5 1.5 0 0 0 0 4.5v10A1.5 1.5 0 0 0 1.5 16h10a1.5 1.5 0 0 0 1.5-1.5V7.864a.5.5 0 0 0-1 0V14.5a.5.5 0 0 1-.5.5h-10a.5.5 0 0 1-.5-.5v-10a.5.5 0 0 1 .5-.5h6.636a.5.5 0 0 0 .5-.5z"/>
           <path fill-rule="evenodd" d="M16 .5a.5.5 0 0 0-.5-.5h-5a.5.5 0 0 0 0 1h3.793L6.146 9.146a.5.5 0 1 0 .708.708L15 1.707V5.5a.5.5 0 0 0 1 0v-5z"/>
